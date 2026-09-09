a20ce5903eb261b33d58c6e19988fe805777fa22
Baladev Krishnan K <baladevkrishnan.k@gmail.com>
2026-09-09 14:13:19 +0530

    Time: 1 ms (99.36%) | Memory: 42.6 MB (71.15%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -1,20 +1,20 @@
 class Solution {
     public long countCommas(long n) {
-        long res=0;
-        if(n<999){
+        if (n < 999) {
             return 0;
-        }else{
-            if(n<1000000){
-                res=n-999;
-            }else if(n<1000000000){
-                res=2*(n-500499);
-            }else if(n<1000000000000L){
-               res=3*(n-333666999L);
-            }else if(n<1000000000000000L){
-                res=4*(n-250250250249L);
-            }else{
-                res=4*(n-250250250249L)+1;
-            }
-        }return res;
+        }
+        if (n < 1000000) {
+            return n - 999;
+        }
+        if (n < 1000000000) {
+            return 2 * (n - 500499);
+        }
+        if (n < 1000000000000L) {
+            return 3 * (n - 333666999L);
+        }
+        if (n < 1000000000000000L) {
+            return 4 * (n - 250250250249L);
+        }
+        return 4 * (n - 250250250249L) + 1;
     }
 }
