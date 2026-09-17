5f0b37e2b7ee45f961930bbcae9c642d0f98d149
Baladev Krishnan K <baladevkrishnan.k@gmail.com>
2026-09-17 20:31:53 +0530

    Time: 14 ms (28.09%) | Memory: 46.1 MB (32.67%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -3,7 +3,7 @@ class Solution {
         s=s.toLowerCase(); 
         s=s.replaceAll("[^a-z0-9]","");
         int n=s.length();
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n/2;i++){
             if(s.charAt(i)!=s.charAt(n-i-1)) return false;
         }return true;
     }
