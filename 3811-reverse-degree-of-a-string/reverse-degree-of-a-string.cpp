2fb75f8d148a4d2c6c4bb0c9b49a8f6450ebe6aa
Baladev Krishnan K <baladevkrishnan.k@gmail.com>
2026-09-21 18:26:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (78.56%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int res=0;
+        for(int i=0;i<s.length();i++){
+            res+=(('z'-s[i]+1)*(i+1));
+        }return res;
+    }
+};
